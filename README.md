e562e4c7e16a75a4283aded9d7e94fa29d414cac
ivofiorentino0@gmail.com <ivofiorentino0@gmail.com>
2021-09-20 12:06:24 -0300

    20/09

---
+++ README.md
@@ -0,0 +1,2 @@
+# MercadoLiebre
+Repository created 
--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 const express = require('express');
 const app = express ();
 const path = require ('path')
-
+const puerto = process.env.PORT || 3000;
 
 const pathPublico = path.join (__dirname,'./public');
 
 app.use(express.static(pathPublico));
 
-app.listen(3000, () => console.log('server up at: http://localhost:3000/'));
+app.listen(puerto, () => console.log('server up at: http://localhost:3000/'));
 
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname,'./views/home.html')));
-
-app.get('/login', (req,res)=> res.sendFile(path.resolve(__dirname + '/views/login.html')));
-
-app.get('/register', (req, res)=> res.sendFile(path.resolve(__dirname + '/views/register.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname,'./views/home.html')));
 
 
 
