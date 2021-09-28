bac57a8d1e982756355df03091b180196404d61b
ivofiorentino0@gmail.com <ivofiorentino0@gmail.com>
2021-09-28 15:11:12 -0300

    Add 28/09

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express ();
 const path = require ('path');
-const port = 3000
+const port = 3001
 
 
 const pathPublico = path.join (__dirname,'./public');
 
 app.use(express.static(pathPublico));
 
-app.listen(process.env.PORT || port, () => console.log ('servidor en puerto 3000'));
+app.listen(process.env.PORT || port, () => console.log ('servidor en puerto 3001'));
 
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname,'./views/home.html')));
 
